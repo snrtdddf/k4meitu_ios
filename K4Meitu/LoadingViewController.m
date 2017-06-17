0956a0600da35ebcf1ea11b48671be1bd99c92b9
snrtdddf <snrtdddf@126.com>
2017-06-17 15:51:23 +0800

    第一版

--- K4Meitu/LoadingViewController.m
+++ K4Meitu/LoadingViewController.m
@@ -17,9 +17,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    
-    
     
     MainTabBarViewController *rooterVC = [[MainTabBarViewController alloc]init];
     UINavigationController *rooterNav = [[UINavigationController alloc]initWithRootViewController:rooterVC];
